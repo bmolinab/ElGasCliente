206c7a6a0c26e7a346d3eccb5d0fd5136f9ef7d9
Luis Alberto Villafuerte <lvillafuerte@digitalstrategy.com.ec>
2018-09-17 13:26:16 -0500

    IOS CERT

--- ElGas/ElGas.iOS/AppDelegate.cs
+++ ElGas/ElGas.iOS/AppDelegate.cs
@@ -5,6 +5,9 @@ using Plugin.FacebookClient;
 using Syncfusion.ListView.XForms.iOS;
 using TK.CustomMap.iOSUnified;
 using UIKit;
+using UserNotifications;
+using WindowsAzure.Messaging;
+
 
 
 namespace ElGas.iOS
@@ -15,6 +18,7 @@ namespace ElGas.iOS
     [Register("AppDelegate")]
     public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
     {
+        private SBNotificationHub Hub { get; set; }
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -32,7 +36,93 @@ namespace ElGas.iOS
             var renderer = new TKCustomMapRenderer();          
             LoadApplication(new App());
             FacebookClientManager.Initialize(app, options);
+
+            if (UIDevice.CurrentDevice.CheckSystemVersion(10, 0))
+            {
+                UNUserNotificationCenter.Current.RequestAuthorization(UNAuthorizationOptions.Alert | UNAuthorizationOptions.Sound | UNAuthorizationOptions.Sound,
+                                                                        (granted, error) =>
+                                                                        {
+                                                                            if (granted)
+                                                                                InvokeOnMainThread(UIApplication.SharedApplication.RegisterForRemoteNotifications);
+                                                                        });
+            }
+            else if (UIDevice.CurrentDevice.CheckSystemVersion(8, 0))
+            {
+                var pushSettings = UIUserNotificationSettings.GetSettingsForTypes(
+                        UIUserNotificationType.Alert | UIUserNotificationType.Badge | UIUserNotificationType.Sound,
+                        new NSSet());
+
+                UIApplication.SharedApplication.RegisterUserNotificationSettings(pushSettings);
+                UIApplication.SharedApplication.RegisterForRemoteNotifications();
+            }
+            else
+            {
+                UIRemoteNotificationType notificationTypes = UIRemoteNotificationType.Alert | UIRemoteNotificationType.Badge | UIRemoteNotificationType.Sound;
+                UIApplication.SharedApplication.RegisterForRemoteNotificationTypes(notificationTypes);
+            }
+
+
             return base.FinishedLaunching(app, options);
+
+
+        }
+
+        public override void RegisteredForRemoteNotifications(UIApplication application, NSData deviceToken)
+        {
+            Hub = new SBNotificationHub(Constants.ListenConnectionString, Constants.NotificationHubName);
+
+            Hub.UnregisterAllAsync(deviceToken, (error) => {
+                if (error != null)
+                {
+                    System.Diagnostics.Debug.WriteLine("Error calling Unregister: {0}", error.ToString());
+                    return;
+                }
+
+                NSSet tags = null; // create tags if you want
+                Hub.RegisterNativeAsync(deviceToken, tags, (errorCallback) => {
+                    if (errorCallback != null)
+                        System.Diagnostics.Debug.WriteLine("RegisterNativeAsync error: " + errorCallback.ToString());
+                });
+            });
+        }
+
+        public override void ReceivedRemoteNotification(UIApplication application, NSDictionary userInfo)
+        {
+            ProcessNotification(userInfo, false);
+        }
+
+        void ProcessNotification(NSDictionary options, bool fromFinishedLaunching)
+        {
+            // Check to see if the dictionary has the aps key.  This is the notification payload you would have sent
+            if (null != options && options.ContainsKey(new NSString("aps")))
+            {
+                //Get the aps dictionary
+                NSDictionary aps = options.ObjectForKey(new NSString("aps")) as NSDictionary;
+
+                string alert = string.Empty;
+
+                //Extract the alert text
+                // NOTE: If you're using the simple alert by just specifying
+                // "  aps:{alert:"alert msg here"}  ", this will work fine.
+                // But if you're using a complex alert with Localization keys, etc.,
+                // your "alert" object from the aps dictionary will be another NSDictionary.
+                // Basically the JSON gets dumped right into a NSDictionary,
+                // so keep that in mind.
+                if (aps.ContainsKey(new NSString("alert")))
+                    alert = (aps[new NSString("alert")] as NSString).ToString();
+
+                //If this came from the ReceivedRemoteNotification while the app was running,
+                // we of course need to manually process things like the sound, badge, and alert.
+                if (!fromFinishedLaunching)
+                {
+                    //Manually show an alert
+                    if (!string.IsNullOrEmpty(alert))
+                    {
+                        UIAlertView avAlert = new UIAlertView("Notification", alert, null, "OK", null);
+                        avAlert.Show();
+                    }
+                }
+            }
         }
 
         public override void OnActivated(UIApplication uiApplication)
