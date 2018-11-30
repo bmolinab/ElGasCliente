20db73feac40570eb9a6fc8283fd447144e59bb1
Néstor Valle <nestorvalle880416@gmail.com>
2018-11-30 11:03:33 -0500

    agragando funcionalidad de recivir notificaciones lower api 26

--- ElGas/ElGas.Android/MyFirebaseMessagingService.cs
+++ ElGas/ElGas.Android/MyFirebaseMessagingService.cs
@@ -15,6 +15,7 @@ using Android.Util;
 using Firebase.Messaging;
 using Android.Media;
 using Android.Graphics;
+using Android.Support.V4.App;
 
 namespace ElGas.Droid
 {
@@ -23,6 +24,8 @@ namespace ElGas.Droid
     public class MyFirebaseMessagingService : FirebaseMessagingService
     {
         const string TAG = "MyFirebaseMsgService";
+
+        private string CHANNEL_ID = "el_gas_cliente_channel_01";
         public override void OnMessageReceived(RemoteMessage message)
         {
             Log.Debug(TAG, "From: " + message.From);
@@ -64,34 +67,89 @@ namespace ElGas.Droid
                         break;
                 }
 
-                SendNotification(msg);
+                if (Build.VERSION.SdkInt >= Build.VERSION_CODES.O)
+                {
+
+                    SendNotification(msg);
+                }
+                else
+                {
+                    SendNotificationLower26(msg);
+                }
 
             }
 
         }
 
+        void SendNotificationLower26(string messageBody)
+        {
+
+            Intent intent = new Intent(this, typeof(MainActivity));
+
+            // Create a PendingIntent; we're only using one PendingIntent (ID = 0):
+            const int pendingIntentId = 0;
+            PendingIntent pendingIntent =
+                PendingIntent.GetActivity(this, pendingIntentId, intent, PendingIntentFlags.OneShot);
+
+            // Instantiate the builder and set notification elements, including pending intent:
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .SetContentIntent(pendingIntent)
+                .SetContentTitle("EL GAS")
+                .SetAutoCancel(true)
+                .SetContentText(messageBody)
+                .SetVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
+                .SetSound(RingtoneManager.GetDefaultUri(RingtoneType.Notification))
+                .SetSmallIcon(Resource.Drawable.ic_launcher)
+                .SetChannelId(CHANNEL_ID);
+
+            // Build the notification:
+            Notification notification = builder.Build();
+
+            // Get the notification manager:
+            NotificationManager notificationManager =
+                GetSystemService(Context.NotificationService) as NotificationManager;
+
+            // Publish the notification:
+            const int notificationId = 0;
+            notificationManager.Notify(notificationId, notification);
+        }
+
         void SendNotification(string messageBody)
         {
-            var intent = new Intent(this, typeof(MainActivity));
-            intent.AddFlags(ActivityFlags.ClearTop);
-            var pendingIntent = PendingIntent.GetActivity(this, 0, intent, PendingIntentFlags.OneShot);
-            long[] v = { 500, 1000 };
+            int notifyID = 1;
+            // The id of the channel. 
+            var importance = NotificationImportance.High;
 
-            var notificationBuilder = new Notification.Builder(this)
-                        .SetContentTitle("El Gas")
-                        .SetSmallIcon(Resource.Drawable.iclauncher)
-                        .SetContentText(messageBody)
-                        .SetAutoCancel(true)
-                        .SetContentIntent(pendingIntent)
-                        .SetVibrate(v)
-                        .SetSound(RingtoneManager.GetDefaultUri(RingtoneType.Notification))
+            NotificationChannel mChannel = new NotificationChannel(CHANNEL_ID, "ELGASCLIENTENOTIFICATIONS", importance);
 
-            ;
+            Intent intent = new Intent(this, typeof(MainActivity));
 
-            var notificationManager = NotificationManager.FromContext(this);
+            // Create a PendingIntent; we're only using one PendingIntent (ID = 0):
+            const int pendingIntentId = 0;
+            PendingIntent pendingIntent =
+                PendingIntent.GetActivity(this, pendingIntentId, intent, PendingIntentFlags.OneShot);
 
+            // Instantiate the builder and set notification elements, including pending intent:
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .SetContentIntent(pendingIntent)
+                .SetContentTitle("EL GAS")
+                .SetAutoCancel(true)
+                .SetVibrate(new long[] { 1000, 1000, 1000, 1000, 1000 })
+                .SetSound(RingtoneManager.GetDefaultUri(RingtoneType.Notification))
+                .SetContentText(messageBody)
+                .SetSmallIcon(Resource.Drawable.ic_launcher)
+                .SetChannelId(CHANNEL_ID);
 
-            notificationManager.Notify(1, notificationBuilder.Build());
+            // Build the notification:
+            Notification notification = builder.Build();
+
+            // Get the notification manager:
+            NotificationManager mNotificationManager =
+             (NotificationManager)GetSystemService(Context.NotificationService);
+            mNotificationManager.CreateNotificationChannel(mChannel);
+
+            // Publish the notification:
+            mNotificationManager.Notify(notifyID, notification);
         }
     }
 }
